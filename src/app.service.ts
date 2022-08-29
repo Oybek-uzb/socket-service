8516a20adc1b7056037c24e76928e1094cc8b01b
Oybek <programmingiswonderful@gmail.com>
2022-08-29 15:11:18 +0500

    db init

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import {DatabaseService} from "./db/database.service";
+import {query} from "express";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private readonly db: DatabaseService) {
+  }
+
+  async getHello(): Promise<string> {
+    const q = "SELECT * FROM users";
+    let exec = await this.db.executeQuery(q);
+    console.log(exec)
     return 'Hello World!';
   }
 }
